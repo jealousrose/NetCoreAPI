51850a616ceebea88b4f623f4429f39be99a8a12
jealousrose <trung2082k3@gmail.com>
2026-01-10 13:38:06 +0700

    baikiemtra

--- DEMO_MVC/obj/Debug/net9.0/DEMO_MVC.AssemblyInfo.cs
+++ DEMO_MVC/obj/Debug/net9.0/DEMO_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DEMO_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c15dd858a69d463b26d9122e271d35a0007d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf34ce2980d531d343511d3161644a79efac58f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DEMO_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DEMO_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
